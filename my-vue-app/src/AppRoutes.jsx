b01a5ca7744f2b5132ff47a608b0421d65d1d9a7
Jan Rylich <114480610+DoubleBlackFox@users.noreply.github.com>
2025-02-20 10:45:30 +0100

    Add files via upload

---
+++ my-vue-app/src/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Route, Routes, BrowserRouter} from "react-router-dom"
+import Home from './pages/Home'
+
+
+export default function AppRoutes() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />}/>
+     
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
